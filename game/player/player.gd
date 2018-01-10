20b10dcdc617667ee3c4f8a3f2033f1a36f8f682
Christian Karcher <contact@ckarcher.de>
2018-01-10 22:15:14 +0100

    added left/right movement

--- game/player/player.gd
+++ game/player/player.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
-var vel = Vector2(100,50)
+const WALK_SPEED = 200
+var vel = Vector2()
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -12,4 +12,14 @@ func _ready():
 	set_process(true)
 	
 func _process(delta):
-	set_pos(get_pos()+vel*delta)
+	# set_pos(get_pos()+vel*delta)
+	vel.y = 0
+
+	if (Input.is_action_pressed("ui_left")):
+		vel.x = -WALK_SPEED
+	elif (Input.is_action_pressed("ui_right")):
+		vel.x =  WALK_SPEED
+	else:
+		vel.x = 0
+	var motion = vel * delta
+	move(motion)
