a0971ce475d8fcb2798396564e354f51a5fdfab2
Christian Karcher <contact@ckarcher.de>
2018-01-18 21:19:22 +0100

    first draft of random river generation

--- scenes/scene1.gd
+++ scenes/scene1.gd
@@ -6,10 +6,17 @@ extends Node2D
 var screensize
 var rivers = randi()%11+1
 var grid_map = []
-var columns = 20
-var rows = 100
+var columns = 50
+var rows = 1000
 var fill_factor = 0.2
 var rand_value = 0
+var riverWidth = [0.5]
+var tempWidth = 0.5
+var riverCentre = [0.5]
+var tempCentre = 0.5
+var centreNoise = 0.1
+var widthNoise = 0.05
+var minWidth = 0.2
 
 onready var plane = get_node("plane")
 
@@ -23,12 +30,29 @@ func generate_grid_map():
 	for x in range(0, columns + 1):
 		grid_map.append([])
 		for y in range(0, rows + 1):
+			 
 			randomize()
-			if rand_range(0, 2) < fill_factor:
-				rand_value = 1
+			if x == 0:
+				tempCentre = riverCentre[y] + rand_range(-1,1)*centreNoise
+				tempCentre = max(tempCentre,riverWidth[y]/2+0.05)
+				tempCentre = min(tempCentre,1-riverWidth[y]/2-0.05)
+				riverCentre.append(tempCentre)
+				tempWidth = riverWidth[y] + rand_range(-1,1)*widthNoise
+				tempWidth = max(minWidth, tempWidth)
+				tempWidth = min(0.9, tempWidth)
+				riverWidth.append(tempWidth)
+				
+			#if rand_range(0, 2) < fill_factor:
+			#	rand_value = 1
+			#else:
+			#	rand_value = 0
+			#grid_map[x].append(rand_value)
+			print(x/columns)
+			print(riverCentre[y]-riverWidth[y]/2)
+			if (float(x)/columns > riverCentre[y]-riverWidth[y]/2) && (float(x)/columns < riverCentre[y]+riverWidth[y]/2):
+				grid_map[x].append(0)
 			else:
-				rand_value = 0
-			grid_map[x].append(rand_value)
+				grid_map[x].append(1)
 			
 func draw_tiles():
 	for x in range(0, columns):
